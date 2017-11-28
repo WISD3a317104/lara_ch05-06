eedb51866a95cba78db91a558dcfe668dc31cc7c
s3a317104 <31909567+s3a317104@users.noreply.github.com>
2017-11-28 21:05:52 +0800

    Route 參數練習

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('student/{student_no}',function($student_no){
+    return "學號:".$student_no;
+});
+Route::get('student/{student_no}/score',function($student_no){
+    return "學號:".$student_no."的所有成績";
+});
+Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
+    return "學號:".$student_no."的".((is_null($subject))?"所有項目":$subject)."成績";
+});
