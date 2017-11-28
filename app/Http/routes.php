880b50f53a413e8b588818cacc946cd48c17319a
s3a317104 <31909567+s3a317104@users.noreply.github.com>
2017-11-28 21:07:54 +0800

    Route參數正規化

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,10 +16,10 @@ Route::get('/', function () {
 });
 Route::get('student/{student_no}',function($student_no){
     return "學號:".$student_no;
-});
+})->where(['student_no'=>'s[0-9]{10}']);
 Route::get('student/{student_no}/score',function($student_no){
     return "學號:".$student_no."的所有成績";
 });
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
     return "學號:".$student_no."的".((is_null($subject))?"所有項目":$subject)."成績";
-});
+})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
