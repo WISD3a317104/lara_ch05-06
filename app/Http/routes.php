527e0bb4530fc67ca1a9f9ce09c5bc070350d4eb
s3a317104 <31909567+s3a317104@users.noreply.github.com>
2017-11-28 21:09:19 +0800

    Route 群組+命名

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,16 +10,23 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+Route::pattern('student_no','s[0-9]{10}');
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('student/{student_no}',function($student_no){
-    return "學號:".$student_no;
-})->where(['student_no'=>'s[0-9]{10}']);
-Route::get('student/{student_no}/score',function($student_no){
-    return "學號:".$student_no."的所有成績";
+Route::group(['prefix'=>'student'],function(){
+    Route::get('{student_no}',[
+        'as'=>'student',
+        'uses'=>function($student_no){
+             return "學號:".$student_no;
+        }
+    ]);
+    Route::get('{student_no}/score/{subject?}',[
+        'as'=>'student.score',
+        'uses'=>function($student_no,$subject=null){
+            return "學號:".$student_no."的".((is_null($subject))?"所有項目":$subject)."成績";
+        }
+    ])->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
 });
-Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject=null){
-    return "學號:".$student_no."的".((is_null($subject))?"所有項目":$subject)."成績";
-})->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
+
+
